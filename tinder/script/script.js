fd0a118a0e05207435248fe2350ef866129a38a9
Ona Demuytere <onademuy@student.arteveldehs.be>
2018-10-07 16:30:24 +0200

    tinder

---
+++ tinder/script/script.js
@@ -0,0 +1,57 @@
+let url='https://randomuser.me/api/';
+
+let people = [];
+let crosses = [];
+let hearts = [];
+
+let picture = document.querySelector("#p_picture"),
+    name = document.querySelector("#p_name"),
+    age = document.querySelector("#p_age");
+
+function updateUser(){
+  fetch(url)
+  .then(function(res){
+    return res.json()
+  })
+  .then(function(data){
+    picture.src = data.results[0].picture.large;
+    name.innerText = data.results[0].name.last + " " + data.results[0].name.first; 
+    age.innerText = data.results[0].dob.age;
+    localStorage.setItem("people", JSON.stringify(people));
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+}
+
+// Buttons en updaten
+let btnX = document.getElementById("btn_cross");
+btnX.addEventListener("click", function(){
+
+    //adds the item to the first in the array
+    crosses.push(people[0]);
+    console.log(crosses);
+    //removes the first item of an array
+    people.shift();
+    //removes the first item of an array from the console
+    localStorage.removeItem("people");
+
+    updateUser();
+
+});
+
+let btnV = document.getElementById("btn_heart");
+btnV.addEventListener("click", function(){
+  
+   //adds the item to the first in the array
+   hearts.push(people[0]);
+   console.log(hearts);
+   //removes the first item of an array
+   people.shift();
+   //removes the first item of an array from the console
+   localStorage.removeItem("people");
+
+   updateUser();
+});
+
+window.onload = updateUser;
